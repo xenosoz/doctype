45ce6ae17e9d2e967f7b2ce0ef683a5f471fb93c
Taihyun Hwang <xenosoz.hwang@gmail.com>
2013-11-26 09:17:19 +0900

    Rebuild context

--- doctype.js
+++ doctype.js
@@ -8,6 +8,7 @@ var $doctype;
     return;
   }
 
+  var $contextMap = {};
   var $moduleMap = {};
   var nodeStack = [];
 
@@ -34,7 +35,7 @@ var $doctype;
         node.addEventListener('load', function() { cleanup(node); onTrue.apply(this, arguments); }, false);
         node.addEventListener('error', function() { cleanup(node); onFalse.apply(this, arguments); }, false);
 
-        console.log("CAS::set" + node.src);
+        console.log("CAS::set " + node.src);
 
         nodeStack.push(node);
         document.head.appendChild(node);
@@ -43,38 +44,102 @@ var $doctype;
     return promise;
   }
 
-  function load(moduleName, url) {
+  function load(moduleName, url, callback) {
     var node = createNode(moduleName, url);
 
     executeNode(node).then(function(event) {
       var node = event.currentTarget || event.srcElement;
+      callback.call(this);
     });
   }
 
-  function Context(contextName) {
+  function Context(name) {
     var self = this;
-    self.$contextName = contextName;
-    self.$scope = {};
 
-    self.$import = function(moduleName) {
-      load(moduleName, moduleName);
-      self.$scope[moduleName] = $moduleMap[moduleName];
-      return self;
+    self.name = name;
+    self.deps = [];
+    self.body = null;
+
+    self.import_ = function(moduleSrc) {
+      self.deps.push([moduleSrc, null, null]);
     };
-    self.$define = function(mainFn) {
-      $moduleMap[contextName] = mainFn.call(this, self.$scope);
-      console.log($moduleMap);
-      return self;
+    self.import_as_ = function(moduleSrc, moduleAlias) {
+      self.deps.push([moduleSrc, null, moduleAlias]);
     };
-  };
+    self.from_import_ = function(moduleSrc, fnSrc) {
+      self.deps.push([moduleSrc, fnSrc, null]);
+    };
+    self.from_import_as_ = function(moduleSrc, fnSrc, fnAlias) {
+      self.deps.push([moduleSrc, fnSrc, fnAlias]);
+    };
+    self.define_ = function(body) {
+      self.body = body;
+    };
+
+    return self;
+  }
+
+  function ContextBuilder(context) {
+    var self = this;
+
+    self.context = context;
+    self.clear = function() {
+      delete self.$import;
+      delete self.$from;
+      delete self.$as;
+    };
+    self.machine = {
+      epsilon: {
+        $import: function(x) { self.$import=x; return self.machine.$import; },
+        $from: function(x) { self.$from=x; return self.machine.$from; },
+        $define: function(x) { self.context.define_(x); self.clear(); return self.machine.$define; },
+      },
+      $import: {
+        $import: function(x) { self.context.import_(self.$import); self.clear(); self.$import=x; return self.machine.$import; },
+        $as: function(x) { self.$as=x; return self.machine.$import$as; },
+        $from: function(x) { self.context.import_(self.$import); self.clear(); self.$from=x; return self.machine.$from; },
+        $define: function(x) { self.context.import_(self.$import); self.clear(); self.context.define_(x); return self.machine.$define},
+      },
+      $import$as: {
+        $import: function(x) { self.context.import_as_(self.$import, self.$as); self.clear(); self.$import=x; return self.machine.$import; },
+        $from: function(x) { self.context.import_as_(self.$import, self.$as); self.clear(); self.$from=x; return self.machine.$from; },
+        $define: function(x) { self.context.import_as(self.$import, self.$as); self.clear(); self.context.define_(x); return self.machine.$define; },
+      },
+      $from: {
+        $import: function(x) { self.$import=x; return self.machine.$from$import; },
+      },
+      $from$import: {
+        $import: function(x) { self.context.from_import_(self.$from, self.$import); self.clear(); self.$import=x; return self.machine.$import; },
+        $as: function(x) { self.$as=x; return self.machine.$from$import$as; },
+        $from: function(x) { self.context.from_import_(self.$from, self.$import); self.clear(); self.$from=x; return self.machine.$from; },
+        $define: function(x) { self.context.from_import_(self.$from, self.$import); self.clear(); self.context.define_(x); return self.machine.$define; },
+      },
+      $from$import$as: {
+        $import: function(x) { self.from_import_as_(self.$from, self.$import, self.$as); self.clear(); $self.$import=x; return self.machine.$import; },
+        $from: function(x) { self.from_import_as_(self.$from, self.$import, self.$as); self.clear(); $self.$from=x; return self.machine.$from; },
+        $define: function(x) { self.from_import_as_(self.$from, self.$import, self.$as); self.clear(); self.context.define_(x); return self.machine.$define; },
+      },
+      $define: {
+      },
+    };
+    return self.machine.epsilon;
+  }
 
   $doctype = function(doctype) {
     var node = nodeStack[nodeStack.length - 1];
-    console.log("CAS: " + node.src);
+    console.log("CAS::get " + node.src);
     var contextName = node.getAttribute('data-doctypemodule');
-    return new Context(contextName);
+    var context = new Context(contextName);
+    $contextMap[contextName] = context;
+
+    return new ContextBuilder(context);
   }
 
-  load('main', 'example/main.js');
+  load('main', 'example/main.js', function() {
+    console.log($contextMap);
+  });
+
+  // XXX
+  $global.$contextMap = $contextMap;
 
 })(this);
